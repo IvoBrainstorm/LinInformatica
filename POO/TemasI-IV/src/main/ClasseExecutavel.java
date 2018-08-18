5277e0605fce2e601c1de8e3fa86ee6601eeaf50
Ivo Matavele <ivofernandomatavele@gmail.com>
2018-08-18 16:16:58 +0200

    O que faltava no commit anterior

---
+++ POO/TemasI-IV/src/main/ClasseExecutavel.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author brainstorm
+ */
+public class ClasseExecutavel {
+    public static void main(String[] args) {
+        
+        ContaCorrente corrente = new ContaCorrente();
+        ContaPoupanca poupanca = new ContaPoupanca();
+        
+        poupanca.depositar(1000);
+        poupanca.setLimite(200);
+        
+        poupanca.sacar(1210);
+        
+        corrente.depositar(4500);
+        corrente.sacar(456);
+        
+    }
+}
